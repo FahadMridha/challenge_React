78d7885a8b32c85d18d0ef63f4d2208bab0783d4
fahad Mridha <sohelcse16@gmail.com>
2023-12-09 23:44:00 +0600

    modal create

---
+++ src/redux/reducers.js
@@ -0,0 +1,13 @@
+import { combineReducers } from "redux";
+import { ADD_TASK } from "./actions";
+
+const taskReducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD_TASK:
+      return [...state, action.payload];
+    default:
+      return state;
+  }
+};
+
+export default combineReducers({ task: taskReducer });
